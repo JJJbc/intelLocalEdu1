8852ff67667a9e678bd0167181e13e52fdc62cce
qudcjf854 <qudcjf854@gmail.com>
2024-05-22 11:01:34 +0900

    Ch5 수업 프로젝트를 등록한다

---
+++ 2.Ch5ArrayBasic/src/ArrayBasic2.java
@@ -0,0 +1,35 @@
+
+public class ArrayBasic2 {
+	public static void main(String[] args) {
+	
+//		배열(Array)
+//	 	같은 타입의 여러 변수를 하나의 묶음으로 다루는 것
+	
+		int a1 = 1;
+		int a2 = 2;
+		int a3 = 3;
+		
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		
+		System.out.println("==============================");
+		int[] numArr = new int [3];
+		
+		System.out.println(numArr[0]);
+		System.out.println(numArr[1]);
+		System.out.println(numArr[2]);
+		
+		numArr[0] = 10;
+		numArr[1] = 20;
+		numArr[2] = 30;
+		
+		System.out.println(numArr[0]);
+		System.out.println(numArr[1]);
+		System.out.println(numArr[2]);
+		
+	
+	
+	
+	}
+}
